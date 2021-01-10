410ba4dd03bb4da8b3e4d5fece9902caee5c1504
rosen316 <rosenjtenev@gmail.com>
2021-01-10 14:16:59 +0200

    switched to relay instead of the built in led for testing purposes

--- Embeded/sta_and_ap_mode/sta_and_ap_mode.ino
+++ Embeded/sta_and_ap_mode/sta_and_ap_mode.ino
@@ -1,112 +1,111 @@
-#include<ESP8266WiFi.h>
-#include<ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
 // WiFi Definitions
-const char* ssid_sta = "jivko";
-const char* password_sta = "jivkottt";
-const char* ssid_ap = "Esp8266AP";
-const char* password_ap = "Esp8266AP";
-const char* value = "";
+String ssid_sta = "jivko";
+String password_sta = "jivkottt";
+String ssid_ap = "Esp8266AP";
+String password_ap = "Esp8266AP";
+String value = "";
+
+uint8_t Relay_Pin = 2;
 
 IPAddress ip(192, 168, 4, 1);
 IPAddress gateway(192, 168, 4, 1);
 IPAddress netmask(255, 255, 255, 0);
 
-ESP8266WebServer server;
+ESP8266WebServer server(80);
 
-void handleRoot() {
-  server.send(200, "text/plain", "hello from esp8266!");
-}
-
-void setup() {
+void setup(void) {
    Serial.begin(115200);
    delay(10);
-   pinMode(LED_BUILTIN, OUTPUT);
-   digitalWrite(LED_BUILTIN, LOW);
+   pinMode(Relay_Pin, OUTPUT);
+   digitalWrite(Relay_Pin, LOW);
    WiFi.mode(WIFI_AP);
    WiFi.softAPConfig(ip, gateway, netmask);
    WiFi.softAP(ssid_ap, password_ap);
-//    WiFi.mode(WIFI_STA);
-//    delay(100);
-//    WiFi.begin(ssid_sta, password_sta);
-      
-   server.on("/led?state=on",ledON);
-   server.on("/led?state=off",ledOFF);
-   server.on("/", handleRoot);
-   server.begin();
-     Serial.println("TEST3");
+
+  server.on("/led", LedSwitch);  
+  server.on("/switch", ModeSwitch);
+  server.on("/config", ConfigSwitch);
+  server.begin();           
+
 }
 
-void loop() {
-//  server.handleClient();
-  // Check if client has connected
-//  WiFiClient client = server.available();
-//  if(!client) {
-//    return;
-//  }
-//
-//  // Read the request line
-//  String request = client.readStringUntil('\r');
-//  Serial.println(request);
-//  client.flush();
-  
-//  // Match request
-//  if(request.indexOf("/led?state=on") != -1) {
-//     digitalWrite(LED_BUILTIN, LOW);
-//     value = "on";
-//  } else if (request.indexOf("/led?state=off") != -1) {
-//     digitalWrite(LED_BUILTIN, HIGH);
-//     value = "off";
-//  } else if (request.indexOf("/switch?mode=ap") != -1) {
-//    WiFi.mode(WIFI_AP);
-//    WiFi.softAPConfig(ip, ip, netmask);
-//    WiFi.softAP(ssid_ap, password_ap);
-//  } else if (request.indexOf("/switch?mode=sta") != -1) {
-//    WiFi.softAPdisconnect();
-//    WiFi.disconnect();
-//    WiFi.mode(WIFI_STA);
-//    delay(100);
-//    WiFi.begin(ssid_sta, password_sta);
-//  }
-  
-//  client.flush();
-//   
-//  // create json response
-//  String s = "HTTP/1.1 200 OK\r\n";
-//  s += "Content-Type: application/json\r\n\r\n";
-//  s += "{\"data\":{\"message\":\"success\",\"value\":\"";
-//  s += value;
-//  s += "\"}}\r\n";
-//  s += "\n";
-//
-//  // Send the response
-//  client.print(s);
-//  delay(1);
-//  Serial.println("Client disconnected");
+void loop(void) {
+  server.handleClient();    
 }
 
-void ledON()
-{
-  Serial.println("TEST1");
-  digitalWrite(LED_BUILTIN, LOW);
-  value = "on";
-  server.send(200,""); 
+void LedSwitch() {
+  if(server.arg("state") == "on")
+  {
+    digitalWrite(Relay_Pin, HIGH);
+    server.send(200, "text/plain", "Led state changed to on");      
+  }
+  else if(server.arg("state") == "off")
+  {
+    digitalWrite(Relay_Pin, LOW);
+    server.send(200, "text/plain", "Led state changed to off") ;   
+  }
+  else
+  {
+    server.send(404, "text/plain", "Route not found");
+  } 
 }
-void ledOFF()
+
+void ModeSwitch() {
+  if(server.arg("mode") == "sta")
+  {
+    WiFi.softAPdisconnect();
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    delay(100);
+    WiFi.begin(ssid_sta, password_sta);
+    server.send(200, "text/plain", "Mode set to station");      
+  }
+  else if(server.arg("mode") == "ap")
+  {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(ip, ip, netmask);
+    WiFi.softAP(ssid_ap, password_ap); 
+    server.send(200, "text/plain", "Mode set to access point");  
+  }
+  else
+  {
+    server.send(404, "text/plain", "Route not found");
+  } 
+}
+
+void ConfigSwitch()
 {
-    Serial.println("TEST2");
-  digitalWrite(LED_BUILTIN, HIGH);
-  value = "off";
-  server.send(200,""); 
-}  
-//String createJsonResponce()
-//{
-//  String s = "HTTP/1.1 200 OK\r\n";
-//  s += "Content-Type: application/json\r\n\r\n";
-//  s += "{\"data\":{\"message\":\"success\",\"value\":\"";
-//  s += value;
-//  s += "\"}}\r\n";
-//  s += "\n";
-//  return s;
-//}
+   if(server.arg("ssid_ap") != "")
+   {
+       ssid_ap = server.arg("ssid_ap");
+   }
+   if(server.arg("password_ap").length() > 7)
+   {
+       password_ap = server.arg("password_ap");
+   }
+   if(server.arg("password_ap") == "none")
+   {
+       password_ap = "";
+   }
+//     if(server.arg("ip_address_ap") != "")
+//   {
+//       ip = ip(192, 168, 4, 1)server.arg("ip_address_ap");
+//   }
+      if(server.arg("ssid_sta") != "")
+   {
+       ssid_sta = server.arg("ssid_sta");
+   }
+   if(server.arg("password_sta").length() > 7)
+   {
+       password_ap = server.arg("password_sta");
+   }
+   if(server.arg("password_sta") == "none")
+   {
+       password_sta = "";
+   }
+   
+}
